9a6e3a74c01b2cd916272aecbf3cbafb44cee7ea
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-26 00:34:02 +0200

    clean up and getting started

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
 
+<p class="text-3xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquam accusamus ab facere, dolorum voluptatum mollitia nemo consectetur earum nulla nihil! Eum voluptas explicabo adipisci ab impedit nesciunt saepe eos!</p>
 
 </template>
 
 
 <script setup lang="ts">
-</script>
+</script> 
