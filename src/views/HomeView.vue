b822019d0960e969ac3e8db9215b29a1d407c13c
ftaskoski123 <filip.taskoski69@gmail.com>
2023-09-27 00:37:32 +0200

    Added error handling for search functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,34 @@
 <template>
-    <main class="container text-white">
-      <div class="pt-4 mb-8 relative">
-        <input
-          type="text"
-          @input="getSearchResults()"
-          v-model="searchQuery"
-          placeholder="Search for a city or state"
-          class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
-        />
-      <ul  class="absolute bg-weather-secondary w-full text-white shadow-md py-2 px-1 top-[66px]" v-if="mapboxSearchResults">
-        <li  v-for="result in mapboxSearchResults" :key="result.id" class="py-2 cursor-pointer">{{ result.place_name }}</li>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        @input="getSearchResults()"
+        v-model="searchQuery"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary w-full text-white shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <p v-if="searchError">Something went wrong. Please try again</p>
+        <p class="py-2" v-if="!searchError && mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li
+            v-for="result in mapboxSearchResults"
+            :key="result.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ result.place_name }}
+          </li>
+        </template>
       </ul>
-      </div>
-    </main>
-  </template>
-
+    </div>
+  </main>
+</template>
 
 <script setup lang="ts">
 import axios from "axios";
@@ -22,22 +36,27 @@ import { ref } from "vue";
 
 const searchQuery = ref<string>("");
 const queryTimeout = ref<number>(0);
-  const mapboxAPIKey =
+const mapboxAPIKey =
   "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
-const mapboxSearchResults = ref<any>(false);
-const getSearchResults = () : void => {
+const mapboxSearchResults = ref<any>(null);
+const searchError = ref<any>(null);
+
+const getSearchResults = (): void => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapboxSearchResults.value = result.data.features;
+      } catch (error) {
+          searchError.value = true; 
+          mapboxSearchResults.value = [];
+      }
       return;
     }
-    mapboxSearchResults.value = false;
+    mapboxSearchResults.value = null;
   }, 300);
 };
-
-</script> 
+</script>
