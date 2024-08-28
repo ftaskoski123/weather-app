1aae3363de71ab234fa0d3e98527a894d3bcbc21
ftaskoski123 <filip.taskoski69@gmail.com>
2024-08-28 10:11:16 -0700

    .

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
+console.log("fdsg");  
 </script>
 
 <style scoped>
